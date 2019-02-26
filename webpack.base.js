94dfba6efefeea6e969f559571bcd26e5855b2a9
ootsuka <sx3072@hotmail.com>
2019-02-26 13:47:25 +0800

    default split chunks

--- webpack.base.js
+++ webpack.base.js
@@ -17,25 +17,7 @@ module.exports = {
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
+      chunks: 'all'
     }
   },
 }
